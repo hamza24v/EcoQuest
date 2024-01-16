7ac3a3a92571da35fe90db00f4febd545e3a033b
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-01-15 21:53:35 -0500

    fixed max percentage overlapping label

--- frontend/src/constant/quests.js
+++ frontend/src/constant/quests.js
@@ -2,5 +2,5 @@ export const quests = [
     { title: 'Recycle 20 plastic bottles', points: 40, label:50},
     { title: 'Reduce water usage by 10%', points: 30, label: 25 },
     { title: 'Plant a tree', points: 50, label: 75 },
-    { title: 'Pick up 10 litter', points: 75, label: 65}
+    { title: 'Pick up 10 litter', points: 75, label: 100}
   ];
